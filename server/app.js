a786c4b3891d0bbbae927f1caf12b5f916816b5e
Scottie <scottie@cccareers.org>
2022-06-23 13:44:24 -0700

    Express Log

--- server/app.js
+++ server/app.js
@@ -1,20 +1,39 @@
 const express = require('express');
+const morgan = require("morgan");
 const fs = require('fs');
 const app = express();
+const csvLog = "./server/log.csv"
+const csvToJson = require("csvtojson")
+
+app.use(morgan("tiny"));
 
 app.use((req, res, next) => {
-// write your logging code here
-
+    let agent = req.headers["user-agent"].replace(",", "");
+    let time = new Date().toISOString();
+    let method = req.method; 
+    let resource = req.originalUrl;
+    let version = req.protocol.toUpperCase() + "/" + req.httpVersion;
+    let status = res.statusCode;
+    let logData = `\n${agent}, ${time}, ${method}, ${resource}, ${version}, ${status}`;
+    fs.appendFile(csvLog, logData, (err) => {
+        if (err) {
+            console.log(err);
+        } 
+    });
+    next();
 });
 
 app.get('/', (req, res) => {
-// write your code to respond "ok" here
-
+    res.status(200).send("ok");
 });
 
 app.get('/logs', (req, res) => {
-// write your code to return a json object containing the log data here
-
+    csvToJson()
+    .fromFile(csvLog)
+    .then((jsonLog) => {
+        res.json(jsonLog);
+        console.log(jsonLog);
+    })
 });
 
 module.exports = app;
